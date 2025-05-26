e8a2224dad4d89a0894cbebfb67298c5c178d24a
HibbaHindawi <hibba04@hotmail.se>
2025-05-26 12:51:30 +0200

    Fixed all sounds

--- src/entity/Characters/Player.js
+++ src/entity/Characters/Player.js
@@ -186,6 +186,14 @@
     this.m_minigunSound;
 
     /**
+     * Sound file for when player gets revived
+     * 
+     * @type {rune.media.Sound}
+     * @private
+     */
+    this.m_reviveSound;
+
+    /**
      * Hitbox for the player
      * 
      * @type {Object}
@@ -210,6 +218,7 @@ panicCity.entity.Player.prototype.init = function () {
     this.m_shootSound = this.application.sounds.sound.get("Shoot-sound");
     this.m_shotgunSound = this.application.sounds.sound.get("Shotgun-sound");
     this.m_minigunSound = this.application.sounds.sound.get("Minigun-sound");
+    this.m_reviveSound = this.application.sounds.sound.get("Revive-sound");
 
     this.m_shootSound.volume = 0.4;
 
@@ -487,6 +496,7 @@ panicCity.entity.Player.prototype.getRessed = function () {
     this.isDowned = false;
     this.rotation = 0;
     this.health += 100;
+    this.m_reviveSound.play();
 }
 
 /**
